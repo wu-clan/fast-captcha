70e91adb61603a84d0068b912bbb030c80e8ed21
wu <jianhengwu0407@gmail.com>
2023-06-09 14:41:41 +0800

    Add base64 encoded string

--- fast_captcha/img_captcha.py
+++ fast_captcha/img_captcha.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+
 import random
 from io import BytesIO
 from pathlib import Path
+from typing import Tuple, Literal
 
 from PIL import Image, ImageDraw, ImageFont
 
 from fast_captcha import text_captcha
 
 
-def use_rgb() -> tuple:
+def use_rgb() -> Tuple[int, ...]:
     """
     random colors for captcha text and distractions
 
@@ -18,44 +21,45 @@ def use_rgb() -> tuple:
     return tuple((random.randint(0, 255) for _ in range(3)))
 
 
-def ttf() -> list:
+def get_ttf() -> list[str]:
     """
     get all *.ttf file
 
     :return:
     """
-    fp = Path(__file__).parent.joinpath('fonts')
-    ft = [str(t) for t in fp.glob('*.ttf')]
-    return ft
+    file_path = Path(__file__).parent.joinpath('fonts')
+    ttf = [str(f) for f in file_path.glob('*.ttf')]
+    return ttf
 
 
 def img_captcha(
-        *,
-        code_num=4,
-        width=120,
-        height=40,
-        font_type=random.choice(ttf()),
-        font_size=32,
-        draw_lines=True,
-        lines_num=4,
-        draw_points=False,
-        points_density=4,
-        img_type='jpeg',
-        img_byte=True) -> tuple:
+    *,
+    code_num: int = 4,
+    width: int = 120,
+    height: int = 40,
+    font_type: str = random.choice(get_ttf()),
+    font_size: int = 32,
+    draw_lines: bool = True,
+    lines_num: int = 4,
+    draw_points: bool = False,
+    points_density: int = 4,
+    img_type: str = 'jpeg',
+    img_byte: Literal['file', 'bytesio', 'base64'] = 'bytesio',
+) -> tuple[Image | BytesIO | bytes, str]:
     """
     img captcha
 
-    :param code_num: number of codes
-    :param width: picture width, default 120
-    :param height: picture height, default 40
-    :param font_size: font display size, default 32
-    :param font_type: captcha font default random
-    :param draw_lines: whether to draw lines
-    :param lines_num: number of lines drawn
-    :param draw_points: whether to draw points
-    :param points_density: draw point's density, the higher the value the higher the density
+    :param code_num: number of codes.
+    :param width: picture width, default 120.
+    :param height: picture height, default 40.
+    :param font_size: font display size, default 32.
+    :param font_type: captcha font default random.
+    :param draw_lines: whether to draw lines.
+    :param lines_num: number of lines drawn.
+    :param draw_points: whether to draw points.
+    :param points_density: draw point's density, the higher the value the higher the density.
     :param img_type: image type, for-example: jpeg, png ...
-    :param img_byte: convert image to bytes, if you use byte stream
+    :param img_byte: convert image to bytes, if you are using byte stream.
     :return:
     """
     # create img
@@ -68,16 +72,24 @@ def img_captcha(
     text = text_captcha(code_num)
     # draw text
     for i, t in enumerate(text):
-        draw.text(xy=(i * width // code_num + random.randint((random.randint(i, code_num)), code_num), code_num / 2),
-                  text=t,
-                  font=font,
-                  fill=use_rgb())
+        draw.text(
+            xy=(i * width // code_num + random.randint((random.randint(i, code_num)), code_num), code_num / 2),
+            text=t,
+            font=font,
+            fill=use_rgb(),
+        )
     # draw lines
     if draw_lines:
         for i in range(lines_num):
-            draw.line(xy=[random.randint(0, width), random.randint(0, height),
-                          random.randint(0, width), random.randint(0, height)],
-                      fill=use_rgb())
+            draw.line(
+                xy=[
+                    random.randint(0, width),
+                    random.randint(0, height),
+                    random.randint(0, width),
+                    random.randint(0, height),
+                ],
+                fill=use_rgb(),
+            )
     # draw points
     if draw_points:
         chance = min(100, max(0, int(points_density)))
@@ -86,12 +98,18 @@ def img_captcha(
                 tmp = random.randint(0, 100)
                 if tmp > 100 - chance:
                     draw.point((w, h), fill=use_rgb())
-    # img type
-    if img_byte:
-        io = BytesIO()
-        img.save(io, img_type)
-        io.seek(0)
-        img = io
-    else:
+    # img save
+    io = BytesIO()
+    img.save(io, img_type)
+    io.seek(0)
+    if img_byte == 'file':
         img = img
+    elif img_byte == 'bytesio':
+        img = io
+    elif img_byte == 'base64':
+        import base64
+
+        img = base64.b64encode(io.getvalue()).decode('utf-8')
+    else:
+        raise ValueError('img_byte must be one of file, bytesio, base64')
     return img, text
--- fast_captcha/text_captcha.py
+++ fast_captcha/text_captcha.py
@@ -11,6 +11,4 @@ def text_captcha(num: int = 4) -> str:
     :param num: number of codes
     :return:
     """
-    return ''.join(
-        (random.choice(string.ascii_letters + string.digits) for _ in range(num))
-    )
+    return ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(num)))
