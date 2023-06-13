4634eb7550eac8d6bf950b57c3a09f528c6fc790
Wu Clan <jianhengwu0407@gmail.com>
2023-06-13 19:37:20 +0800

    🔖 Release version 0.2.1

--- fast_captcha/__init__.py
+++ fast_captcha/__init__.py
@@ -4,6 +4,6 @@ from .text_captcha import text_captcha as text_captcha
 from .img_captcha import img_captcha as img_captcha
 from .img_captcha import get_ttf as get_ttf
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 __all__ = ['text_captcha', 'get_ttf', 'img_captcha']
